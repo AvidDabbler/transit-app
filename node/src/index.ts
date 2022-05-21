b2222c71f2d8690483e407b24e4a6a05bd9d958b
aviddabbler <walter.k.jenkins@gmail.com>
2022-05-20 19:19:20 -0500

    updated ignore

---
+++ node/src/index.ts
@@ -0,0 +1,22 @@
+
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+export const env = dotenv.config().parsed;
+const app = express();
+const port = env?.PORT;
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+	res.send('Welcome to my silly transit app');
+});
+
+app.get('/routes', (req, res) => {
+	res.send('Welcome to routes');
+});
+
+app.listen(4000, async () => {
+	console.log(`Example app listening on port ${port}`);
+});
