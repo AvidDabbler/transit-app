86d7f3a4a41d2575823d9b9f68d98e7b9bcb95db
aviddabbler <walter.k.jenkins@gmail.com>
2022-05-21 16:22:16 -0500

    added initial list

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,17 +1,35 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.scss'
-import { Phone } from './components/phone/Phone'
-import { Collapse } from './components/Collapse'
+import { Fragment, useState } from 'react';
+import logo from './logo.svg';
+import './App.scss';
+import { Phone } from './components/phone/Phone';
+import { Collapse } from './components/Collapse';
 
 function App() {
-  return (
-    <div className="App">
-      <Phone>
-        <Collapse text='Hey there' />
-      </Phone>
-    </div>
-  )
+	return (
+		<div className="App">
+			<Phone>
+				<Fragment>
+					<div className="map"></div>
+					<div className="bottom-list">
+						<Collapse text="Hey there">
+              <h1>Walter!!!</h1>
+            </Collapse>
+						<Collapse text="Hey there" />
+						<Collapse text="Hey there" />
+						<Collapse text="Hey there" />
+						<Collapse text="Hey there" />
+						<Collapse text="Hey there" />
+						<Collapse text="Hey there" />
+						<Collapse text="Hey there" />
+						<Collapse text="Hey there" />
+						<Collapse text="Hey there" />
+						<Collapse text="Hey there" />
+						<Collapse text="Hey there" />
+					</div>
+				</Fragment>
+			</Phone>
+		</div>
+	);
 }
 
-export default App
+export default App;
--- front/src/components/Collapse.tsx
+++ front/src/components/Collapse.tsx
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { Children, useState } from 'react';
+import { RiArrowDownSLine } from 'react-icons/ri';
+import { Collapse as Collapsable } from 'react-collapse';
 
-interface CollapseType{
+interface CollapseType {
 	text: string;
+	children: JSX.Element;
 }
 
-export const Collapse = ({text}:CollapseType) => {
+export const Collapse = ({ text, children }: CollapseType) => {
+	const [isOpen, setIsOpen] = useState<boolean>(false);
+
 	return (
-		<div className='collapse'>
-			<h2 className='h2'>{text}</h2>
+		<div>
+			<div className="collapse">
+				<h2 className="h2">{text}</h2>
+				<RiArrowDownSLine
+					onClick={() => setIsOpen(!isOpen)}
+					className="carrot"
+					size={30}
+				/>
+			</div>
+			<Collapsable isOpened={isOpen}>{children}</Collapsable>
 		</div>
-	)
-}
+	);
+};
