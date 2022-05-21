6821235c9711e45e38b8d31275a5b62e9a22780e
aviddabbler <walter.k.jenkins@gmail.com>
2022-05-21 13:34:43 -0500

    setup init libs and router

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import App from './App'
 import './index.css'
+import { NoPage } from './Roues/NoPage'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="*" element={<NoPage />} />
+    </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 )
---
+++ front/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    colors: {
+      white: '#ffffff',
+      alabaster: '#F6EFE7',
+      ltOrange: '#F6D69F',
+      orange: '#FDAF29',
+      dkBlue: '#1B293D',
+      blue: '#1A5DB4',
+      darkened: 'rgba(102, 102, 102, 0.384)',
+      darkPurple: '#24283d',
+      black: '#000000',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
